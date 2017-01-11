530b056ec770efccb7ca29cf481a1c046e566eb5
fgh <fgh@gmail.com>
2017-01-12 05:42:47 +0800

    jiaruxianshitianqixinxidegongneng.

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by 符国豪 on 2017/1/12.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
